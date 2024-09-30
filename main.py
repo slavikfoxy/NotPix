9a4bc3659f91bd6b0930d93991236b238bb1c4f1
slavikfoxy <oliynik.v2014@gmail.com>
2024-09-30 08:27:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ from bot.utils.launcher import process
 
 
 async def main():
-    # Встановлюємо тайм-аут на 10 секунд (можна змінити час)
     try:
-        await asyncio.wait_for(process(), timeout=270.0)
+        await asyncio.wait_for(process(), timeout=10.0)  # Тайм-аут 10 секунд
     except asyncio.TimeoutError:
         logger.warning("<r>Bot stopped due to timeout...</r>")
-        sys.exit(2)
+        # Можна коректно завершити роботу без виклику sys.exit(1)
+    except Exception as e:
+        logger.error(f"<r>Unexpected error: {e}</r>")
+        sys.exit(1)  # Завершуємо через помилку
 
 
 if __name__ == '__main__':
