623ea93f3dfbc8c7cc95dd14349c847ded9aa37e
slavikfoxy <oliynik.v2014@gmail.com>
2024-10-26 21:28:51 +0200

    difference_pix

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -59,6 +59,7 @@ class Tapper:
         self.updated_pixels = {}
         self.socket = None
         self.socket_task = None
+        self.last_balance = 0
 
         self.session_ug_dict = self.load_user_agents() or []
 
@@ -411,8 +412,23 @@ class Tapper:
         draw_request.raise_for_status()
 
         data = await draw_request.json()
-
-        self.success(f"Painted (X: <cyan>{x}</cyan>, Y: <cyan>{y}</cyan>) with color <light-blue>{color}</light-blue> 🎨️ | Balance <light-green>{'{:,.3f}'.format(data.get('balance', 'unknown'))}</light-green> 🔳")
+        balance = data.get('balance', 'unknown')
+        difference_pix = 0
+        try:
+            if isinstance(balance, str):
+                balance = float(balance.replace(',', ''))  
+            elif isinstance(balance, (int, float)):
+                balance = float(balance) 
+            difference_pix = balance - self.last_balance
+            
+            formatted_balance = '{:,.3f}'.format(balance)
+            formatted_difference_pix = '{:,.3f}'.format(difference_pix)
+            
+            self.last_balance = balance
+            
+        except ValueError:
+            self.error(f"ValueError, error convert difference_pix to float.")
+        self.success(f"Painted (X: <cyan>{x}</cyan>, Y: <cyan>{y}</cyan>) with color <light-blue>{color}</light-blue> 🎨️ | Balance <light-green>{'{:,.3f}'.format(data.get('balance', 'unknown'))}</light-green> <red>(+ {round(difference_pix)} pix) </red>  🔳")
 
     async def draw_x3(self, http_client: aiohttp.ClientSession):
         try:
@@ -475,7 +491,9 @@ class Tapper:
             response = await http_client.get('https://notpx.app/api/v1/mining/status', ssl=settings.ENABLE_SSL)
             response.raise_for_status()
             data = await response.json()
-            charges = data['charges']
+            charges = data['charges'] 
+            balance_str = data.get('userBalance', 'unknown')
+            self.last_balance = float(balance_str)
 
             if charges > 0:
                 self.info(f"Energy: <cyan>{charges}</cyan> ⚡️")
@@ -483,6 +501,8 @@ class Tapper:
                 self.info(f"No energy ⚡️")
                 return None
 
+            x_offset, y_offset = settings.X_OFFSET, settings.Y_OFFSET  # Координат шаблону
+            
             # Download Image
             if settings.DOWNLOAD_METHOD_2 and not settings.DOWNLOAD_FROM_FILE:
                 self.info(f"Способ загрузки шаблона - urllib.request")
@@ -508,7 +528,6 @@ class Tapper:
                 original_image_url = settings.IMAGE_LINK
                 pattern = r'://([^/]+)/'
                 match = re.search(pattern, original_image_url)
-                x_offset, y_offset = settings.X_OFFSET, settings.Y_OFFSET  # Координат шаблону
                 image_headers = deepcopy(headers)
                 image_headers['Host'] = match.group(1)
                 original_image = await self.get_image(http_client, original_image_url, image_headers=image_headers)
