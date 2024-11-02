f1ba40bfbff54a6e9f7cf403716f328cb609f238
slavikfoxy <oliynik.v2014@gmail.com>
2024-11-02 16:22:29 +0100

    Old my version

--- bot/core/registrator.py
+++ bot/core/registrator.py
@@ -25,7 +25,15 @@ async def register_sessions() -> None:
     API_ID = settings.API_ID
     API_HASH = settings.API_HASH
 
-@@ -37,8 +19,6 @@ async def register_sessions() -> None:
+    if not API_ID or not API_HASH:
+        raise ValueError("API_ID and API_HASH not found in the .env file.")
+
+    session_name = input('\nEnter the session name (press Enter to exit): ')
+
+    if not session_name:
+        return None
+
+    session = Client(
         name=session_name,
         api_id=API_ID,
         api_hash=API_HASH,
@@ -33,3 +41,8 @@ async def register_sessions() -> None:
         device_model=android_device,
         workdir="sessions/"
     )
+
+    async with session:
+        user_data = await session.get_me()
+
+    logger.success(f'Session added successfully @{user_data.username} | {user_data.first_name} {user_data.last_name}')
